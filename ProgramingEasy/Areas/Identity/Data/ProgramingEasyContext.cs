ef51c9776c12976e4fe2ed6ddb3301972b502f79
Marlon Muñoz <Marlon Muoz@DESKTOP-4TJLGGT>
2019-12-07 23:53:31 -0600

    Version 2.0

---
+++ ProgramingEasy/Areas/Identity/Data/ProgramingEasyContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProgramingEasy.Areas.Identity.Data;
+
+namespace ProgramingEasy.Models
+{
+    public class ProgramingEasyContext : IdentityDbContext<ProgramingEasyUser>
+    {
+        public ProgramingEasyContext(DbContextOptions<ProgramingEasyContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ ProgramingEasy/Areas/Identity/Data/ProgramingEasyUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ProgramingEasy.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the ProgramingEasyUser class
+    public class ProgramingEasyUser : IdentityUser
+    {
+        [PersonalData]
+        public string Name { get; set; }
+        [PersonalData]
+        public DateTime DOB { get; set; }
+    }
+}
